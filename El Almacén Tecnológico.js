6be22d1ddd8d515b3263f8bce150921dd484e702
dylan <dylangordillomora16@gmail.com>
2026-01-09 02:40:25 -0500

    Ya tenemos casi todo

---
+++ El Almacén Tecnológico.js
@@ -0,0 +1,35 @@
+let inventario = [
+    { nombre: "Laptop", precio: 1000, stock: 5 },
+    { nombre: "Mouse", precio: 25, stock: 0 },
+    { nombre: "Teclado", precio: 50, stock: 12 },
+    { nombre: "Monitor", precio: 200, stock: 3 },
+    { nombre: "Audífonos", precio: 80, stock: 0 }
+];
+ 
+// Filtrado
+function filtrado_Disponiple (inventario) {
+    let disponiples = []
+    const logitud = inventario.length
+    for( let i = 0 ; i < logitud ; i++) {
+         if(inventario[i].stock > 0) {
+             disponiples.push(inventario[i])
+             console.log(disponiples)
+         }
+    }
+}
+
+filtrado_Disponiple(inventario)
+
+// Calcular el precio total del inventario
+function CalcualarValorTotal (inventario) {
+        let valortotal = 0
+    for(let i = 0 ; i < inventario.length ; i++) {
+        let multiplicacion  = inventario[i].precio * inventario[i].stock
+        valortotal += multiplicacion
+    }
+    console.log(`Precio final  del inventario  : ${valortotal}`)
+}
+
+CalcualarValorTotal(inventario)
+
+//
---
+++ documetacion.md
@@ -0,0 +1,22 @@
+## El Almacén Tecnológico
+
+un inventario  donde cada producto es un objeto
+
+nuetro inventrario  
+  es el  siguente
+```js
+  let inventario = [
+    { nombre: "Laptop", precio: 1000, stock: 5 },
+    { nombre: "Mouse", precio: 25, stock: 0 },
+    { nombre: "Teclado", precio: 50, stock: 12 },
+    { nombre: "Monitor", precio: 200, stock: 3 },
+    { nombre: "Audífonos", precio: 80, stock: 0 }
+];
+```
+
+ Nuestras Misiones es 
+
+ A Filtrado   // hecho 
+ B Calculo  // hecho
+ C Trasformacion
+ D Búsqueda Crítica
